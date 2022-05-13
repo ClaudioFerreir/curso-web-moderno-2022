50aef9a3e0ccffd8f93ab98594d188a565e9f845
claudioferreira <claudioferreirajr@hotmail.com>
2022-05-13 12:21:08 -0300

    pares e impares

--- exercicios-js/fundamentos/controle/paresEimpares.js
+++ exercicios-js/fundamentos/controle/paresEimpares.js
@@ -1,1 +1,9 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 == 0) {
+    console.log(`${nums[i]} é numero par`)
+  }else{
+    console.log(`${nums[i]} é numero impar`)
+  }
+}
